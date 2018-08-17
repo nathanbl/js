9e9257d92a5cd4f318bb2a78e29335afce99cff6
nathanbl <nhubadao@gmail.com>
2018-08-17 22:12:52 +0700

    Create y.js

---
+++ y.js
@@ -0,0 +1,269 @@
+var MOVIE_NAME = [
+    ['gười đà', 180, 100],
+    ['ọ lem th', 180, 100]
+]
+        
+var _url = location.href
+var isKeyDown = false;
+var isForcePause = false;
+var qualities = ['hd2160', 'hd1440', 'hd1080', 'hd720', 'large', 'medium', 'small', 'tiny', 'auto'];
+
+window.p = null;
+window.vid = null;
+window.my_played = '0';
+var i___1 = null;
+var i___2 = null;
+var video_height = 'height:720px;width:1280px;'
+var video_height = 'height:1080px;width:1920px;'
+var video_height = 'height:1050px;width:1867px;'
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var eventify = function eventify(self) {
+    self.events = {};
+
+    self.on = function (event, listener) {
+        if (_typeof(self.events[event]) !== 'object') {
+            self.events[event] = [];
+        }
+
+        self.events[event].push(listener);
+    };
+
+    self.removeListener = function (event, listener) {
+        var idx = void 0;
+
+        if (_typeof(self.events[event]) === 'object') {
+            idx = self.events[event].indexOf(listener);
+
+            if (idx > -1) {
+                self.events[event].splice(idx, 1);
+            }
+        }
+    };
+
+    self.emit = function (event) {
+        var i,
+            listeners,
+            length,
+            args = [].slice.call(arguments, 1);
+
+        if (_typeof(self.events[event]) === 'object') {
+            listeners = self.events[event].slice();
+            length = listeners.length;
+
+            for (i = 0; i < length; i++) {
+                listeners[i].apply(self, args);
+            }
+        }
+    };
+
+    self.once = function (event, listener) {
+        self.on(event, function g() {
+            self.removeListener(event, g);
+            listener.apply(self, arguments);
+        });
+    };
+};
+
+var myEmitter = {}
+eventify(myEmitter)
+//myEmitter.on('video_end', function() {
+//})
+
+//(function(document, window) {
+window.setTimeout(function() {
+    function insertAfter(el, referenceNode) {
+        referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+    }
+    function insertBefore(el, referenceNode) {
+        referenceNode.parentNode.insertBefore(el, referenceNode);
+    }
+    if (_url.indexOf('m.youtube.com') != -1) {
+        //if (_url.indexOf('watch?v=') == -1) {
+        //    return;
+        //}
+        if (_url.indexOf('playlist?list=') != -1) {
+            document.title = document.querySelector('.playlist-header .section h2').innerText
+        }
+        
+        function getPlayer() {
+            window.p = document.getElementById('movie_player') ||
+                document.getElementById('movie_player-flash');
+            if(window.p) window.vid = window.p.querySelector('video')
+            // console.log(window.p)
+            _url = location.href
+            /*if (_url != location.href) {
+                _url = location.href
+                window.my_played = 'on url diff';
+            }*/
+        }
+        function startPlayer() {
+            if (_url.indexOf('watch?v=') == -1) {
+                return;
+            }
+            if (!window.p) {
+                console.log('not found video')
+                return;
+            }
+            console.log('window.my_played', window.my_played)
+            if (window.my_played == window.vid.src) {
+                console.log('startPlayer old')
+                if (window.vid.paused) {
+                    //window.p.playVideo()
+                }
+                return;
+            }
+            console.log('startPlayer new')
+            
+            //if (window.p) clearInterval(i___1)
+            console.log(window.p, window.p.getVideoData(), window.p.getDuration())
+            
+            //window.p.pauseVideo();
+            
+            document.body.setAttribute('style','background: #000;')
+            document.documentElement.setAttribute('style','background: #000;')
+            document.querySelector('#player').setAttribute('style',video_height)
+            document.querySelector('.player-container').setAttribute('style','right:0')
+            window.p.setAttribute('style',video_height) //  + 'border: 5px solid green'
+            window.p.querySelector('video').setAttribute('style',video_height)
+            
+            setTimeout(function() {
+                document.querySelectorAll('.page-container ytm-single-column-watch-next-results-renderer ytm-item-section-renderer')[1].setAttribute('style', 'position:absolute;right:0;top:1080px;background:#fff;display:block;z-index:99999')
+            }, 2000)
+            
+            var data = window.p.getVideoData()
+            document.title = '@' + data['title'] + ' - Youtube'
+            
+            var qCur = window.p.getPlaybackQuality()
+            var qSet = 'hd720'
+            if (qCur != qSet) {
+                window.p.setPlaybackQuality(qSet);
+                try {
+                    window.p.setPlaybackQualityRange(qSet, qSet);
+                } catch(e) {}
+                document.title = '@720-' + data['title'] + ' - Youtube'
+            }
+            //document.title = document.querySelector('.video-main-content-title').innerText + ' - Youtube'
+            
+            var cMovieName = '######';
+            var cMovieIntro = 0;
+            var cMovieEnd = 0;
+            for(var i = 0; i < MOVIE_NAME.length; i++) {
+                var _movie = MOVIE_NAME[i];
+                console.log('_movie', _movie)
+                cMovieName = _movie[0]
+                cMovieIntro = _movie[1]
+                cMovieEnd = _movie[2]
+                if (data['title'].toLowerCase().indexOf(cMovieName) != -1) {
+                    break;
+                }
+            }
+            
+            if (cMovieIntro != 0) {
+                console.log('seek to', cMovieIntro)
+                window.p.seekTo(cMovieIntro);//p.pauseVideo()
+            }
+            
+            window.p.playVideo()
+            
+            setTimeout(function() {
+                if (!window.vid.paused) {
+                    window.my_played = window.vid.src;
+                    console.log('window.vid.src', window.my_played)
+                } else {
+                    window.p.playVideo()
+                }
+            }, 200)
+            
+            var dur = window.p.getDuration()
+            i___2 = setInterval(function() {
+                if(!window.p) {
+                    return
+                }
+                var cur = window.p.getCurrentTime()
+                if (cur >= (dur - cMovieEnd)) {
+                    window.my_played = 'on END';
+                    window.p.pauseVideo();
+                    
+                    if (_url.indexOf('&list=') != -1) {
+                        document.querySelectorAll('ytm-playlist-controls .playlist-controls-primary c3-material-button')[1].querySelector('button').click()
+                    } else {
+                        window.p.nextVideo()
+                    }
+                    window.p = null;
+                    //clearInterval(i___1)
+                    i___1 = setInterval(function() {
+                        // console.log('========== on end video setInterval', i___1, i___2)
+                        startPlayer()
+                    }, 2000)
+                    //clearInterval(i___2)
+                }
+            }, 1000)
+        }
+        
+        i___0 = setInterval(function() {
+            //console.log('getPlayer', i___0)
+            getPlayer()
+        }, 1000)
+        
+        i___1 = setInterval(function() {
+            //console.log('startPlayer', i___1)
+            startPlayer()
+        }, 2000)
+        
+        
+        
+    } else {
+        if (_url.indexOf('watch?v=') == -1) {
+            return;
+        }
+        var div = document.createElement("div");
+        div.setAttribute('id', 'my_fixed_div')
+        div.setAttribute('style', 'position: absolute; top: 8em; left: 9em; z-index: 9999; background: #ccc; width: 64em; height: 25em;')
+        div.innerHTML = '<h1 style="font-size: 5em;">### Click or Esc to hide ###<br>Alt + click to PAUSE</h1>';
+        div.addEventListener('click', function() {
+            console.log(this)
+            this.style.display = 'none';
+            this.setAttribute('id', 'my_fixed_div_HIDE')
+        }, false)
+        document.querySelector('body').appendChild(div)
+        
+        setTimeout(function() {
+            div.style.height = '36em';
+        }, 8 * 1000)
+        
+        window.addEventListener("keydown", e => {
+            console.log('keydown:', e)
+            /*
+            if (isKeyDown) {
+                e.preventDefault();
+                return;
+            }*/
+            isKeyDown = true;
+            if (e.keyCode == 27) { // esc == close gallery and DELETE FILE !!!!!!!!!!!!!
+                e.preventDefault();
+                var dh = document.querySelector('#my_fixed_div_HIDE')
+                console.log('dh:', dh)
+                if (dh) {
+                    dh.style.display = 'block';
+                    dh.setAttribute('id', 'my_fixed_div')
+                } else {
+                    var d = document.querySelector('#my_fixed_div')
+                    if (d) {
+                        d.style.display = 'none';
+                        d.setAttribute('id', 'my_fixed_div_HIDE')
+                    }
+                }
+            }
+            if (e.altKey) {
+                isForcePause = true;
+            }
+        }, false);
+        window.addEventListener("keyup", e => {
+            isKeyDown = false;
+        }, false);
+    }
+//})(document, window);
+}, 1000)
+
